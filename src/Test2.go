a279860da8a44c044a4f8ea6cb792431501841d5
QNattaponQ <sunshiro9@gmail.com>
2017-11-01 16:21:01 +0700

    Q initial 1

--- src/Test2.go
+++ src/Test2.go
@@ -20,7 +20,7 @@ import (
 )
 
 /*
-adddd
+adddddkdkdkdkdkdkdkdkdkdkdkdkdkdkdkd
 func ErrorWithJSON(w http.ResponseWriter, json []byte, code int) {
 	var uuid, _ = newUUID()
 	jobid := strconv.Itoa(randInt())
